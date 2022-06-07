eda92507f9e1059ffd6d1deb090c2e7ba2771c91
rashkol <rashitpine@gmail.com>
2022-06-07 18:14:23 +0300

    new commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿class Solution
+{
+    public static void Main(string[] args)
+    {
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
+    
+    if (N % 2 == 1 || N >=6 && N <=20 )
+    {
+       Console.WriteLine("Weird");
+    }
+    else
+    { 
+       Console.WriteLine("Not Weird");
+    }
+    }
+}
